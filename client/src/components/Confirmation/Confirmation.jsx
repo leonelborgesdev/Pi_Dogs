a2054b3b6282f0c3391f99eaeda46565b75add0c
leonelborgesdev <leonel03t@gmail.com>
2022-08-05 01:00:13 -0400

    confirm

--- client/src/components/Confirmation/Confirmation.jsx
+++ client/src/components/Confirmation/Confirmation.jsx
@@ -1,21 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Confir from "../../assets/Confir.gif";
+import "./Confirmation.css";
 
 export const Confirmation = () => {
   const navigate = useNavigate();
+  const { message } = useSelector((state) => state);
   return (
     <div>
       <h1>Confirmation</h1>
       <img src={Confir} />
-      <div className="group_btn">
-        <button
-          onClick={() => {
-            navigate("/breeds");
-          }}
-        >
-          Aceptar
-        </button>
+      <div className="container_confirm">
+        <h3 className="label_h3">{message}</h3>
+        <div className="group_btn">
+          <button
+            onClick={() => {
+              navigate("/breeds");
+            }}
+          >
+            Aceptar
+          </button>
+        </div>
       </div>
     </div>
   );
