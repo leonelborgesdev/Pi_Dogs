9dcbd06c4d8cc7e47726d565dcdce5bbe706d701
leonelborgesdev <leonel03t@gmail.com>
2022-08-05 00:36:21 -0400

    confirmation

--- client/src/components/Confirmation/Confirmation.jsx
+++ client/src/components/Confirmation/Confirmation.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Confir from "../../assets/Confir.gif";
 
 export const Confirmation = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Confirmation</h1>
       <img src={Confir} />
+      <div className="group_btn">
+        <button
+          onClick={() => {
+            navigate("/breeds");
+          }}
+        >
+          Breeds
+        </button>
+      </div>
     </div>
   );
 };
